909143d4c20ddbb99ffc3675ff082ec4d265f70c
Vanessa Toro <vanetoro84@gmail.com>
2018-05-02 15:56:55 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,8 +2,8 @@ class Show < ActiveRecord::Base
 
 
   def self.highest_rating
-    binding.pry
-    self.maximum('highest_rating')
+    # binding.pry
+    Show.maximum('highest_rating')
   end
 
 end
