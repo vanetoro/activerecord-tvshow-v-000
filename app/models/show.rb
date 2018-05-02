3bf0751b6606f64daffa1a3612fb5a84b97b35fb
Vanessa Toro <vanetoro84@gmail.com>
2018-05-02 16:12:57 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
     Show.maximum('rating')
   end
   def self.most_popular_show
-    binding.pry
+    # binding.pry
     Show.where('rating = ?', highest_rating)
   end
 end
