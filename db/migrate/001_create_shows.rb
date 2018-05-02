8f5930ec7fcb385afe772da3231319d5c51dba5d
Vanessa Toro <vanetoro84@gmail.com>
2018-05-02 15:31:06 +0000

    automatically backed up by learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,9 @@
+class CreateShows < ActiveRecord::Migration
+  def change
+    create_table shows: do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+
+end
